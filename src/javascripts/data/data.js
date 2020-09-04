8f70905d72508c14d24852e4eba91bcf26ff1070
bandstrar <rob.bandstra@gmail.com>
2020-09-04 11:49:21 -0500

    dryed up printToDom

--- src/javascripts/data/data.js
+++ src/javascripts/data/data.js
@@ -1,19 +1,35 @@
 const valueData = [
   {
-    name: 'full',
-    value: 100,
+    name: 'eat',
+    full: 100,
+    button1: 10,
+    button2: -3,
+    button1Name: 'Healthy Food',
+    button2Name: 'Junk Food'
   },
   {
-    name: 'fun',
-    value: 50,
+    name: 'play',
+    fun: 50,
+    button1: 50,
+    button2: 2,
+    button1Name: 'Go Skydiving!',
+    button2Name: 'Go for a Walk'
   },
   {
-    name: 'strength',
-    value: 100,
+    name: 'fight',
+    strength: 100,
+    button1: 1,
+    button2: -10,
+    button1Name: 'Run Away!',
+    button2Name: 'Brawl!'
   },
   {
-    name: 'energy',
-    value: 50,
+    name: 'sleep',
+    energy: 50,
+    button1: 50,
+    button2: 60,
+    button1Name: 'Take a Nap',
+    button2Name: 'Deep Slumber'
   },
 ];
 
